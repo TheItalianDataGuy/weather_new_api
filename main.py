f81b2e6e7dc74bb97f34ae036783d70ef8c454e3
Andrew <andreamarella0@gmail.com>
2024-05-31 20:29:51 +0100

    Add URL endpoints for yearly, all data

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 import pandas as pd
-
-
+from datetime import datetime
 
 # Create a website object
 app = Flask(__name__)
@@ -9,6 +8,7 @@ app = Flask(__name__)
 # Table to be visualised on the home webpage
 stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 stations = stations[["STAID", "STANAME                                 "]]
+
 
 # Connect html pages to the website object (DECORATOR)
 @app.route("/")
@@ -19,7 +19,7 @@ def home():
     return render_template("home.html", data=stations.to_html())
 
 
-# By using <statio> <date> you make this variable dynamic
+# By using <station> <date> you make this variable dynamic
 # So the users can insert the station and date they want
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
@@ -33,6 +33,26 @@ def about(station, date):
             "date": date,
             "temperature": temperature}
 
+
+# Create the option to select a specific station and get all the data with years and temperatures
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+# Crete URL based on the date.
+# '    DATE' needs to be converted into a string with .astype() function
+# .str.startswith(str(year)) will select the year typed in
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 # If run multiple Flask apps, change the port (default port is 5000)
 if __name__ == "__main__":
